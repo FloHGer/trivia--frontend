90a2fc59104c9e019edbeb216b16e50d9c242920
FloHGer <mail@florianhoehle.de>
2022-02-01 17:48:44 +0100

    Test my old version

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,59 +1,151 @@
-import {useEffect, useState} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
 
-import Spinner from '../components/common/Spinner';
-import Category from '../components/Game/GameCategory';
 import Question from '../components/Game/Question';
-import { useCategories, useShowQuestion, useLoading, useAnswers } from "../context/gameContext";
-
-import classes from './../sass/pages/Game.module.scss';
+import {useCategories} from '../context/gameContext'
+import Spinner from '../components/common/Spinner';
 
 export default function Game() {
-	const [selectedCategories] = useCategories();
-	const [showQuestion] = useShowQuestion();
-	const [isLoaded] = useLoading();
-	const [allAnswers, setAllAnswers] = useAnswers();
+	const [selectedCategories, setSelectedCategories] = useCategories();
 
-	const [isLoading, setIsLoading] = useState(false); // set to true after testing
-	const [score, setScore] = useState(0);
+  const [score, setScore] = useState(0);
+  const [questions, setQuestions] = useState();
+  const [allAnswers, setAllAnswers] = useState([
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+  ]);
+  const [showQuestion, setShowQuestion] = useState(null);
+  const allRefs = useRef([[], [], [], [], [], []]);
+  
+  const setRef = (i, number, ref) => {
+    if (ref) allRefs.current[i][number] = ref;
+  };
 
-	useEffect(() => {
-		if(isLoaded.length === 6) setIsLoading(false)
-	}, [isLoaded]);
-	
+  // fetch questions
+  useEffect(() => { 
+    let cats = {};
+    for (let i = 0; i < selectedCategories.length; i++) {
+      (async () => {
+        const easy = (
+          await axios(
+            `https://opentdb.com/api.php?amount=2&category=${selectedCategories[i].id}&difficulty=easy`
+          )
+        ).data.results;
+        const medium = (
+          await axios(
+            `https://opentdb.com/api.php?amount=2&category=${selectedCategories[i].id}&difficulty=medium`
+          )
+        ).data.results;
+        const hard = (
+          await axios(
+            `https://opentdb.com/api.php?amount=1&category=${selectedCategories[i].id}&difficulty=hard`
+          )
+        ).data.results;
+        cats[i] = [...easy, ...medium, ...hard];
+      })();
+    }
+    setQuestions(cats);
+  }, [selectedCategories]);
 
-	// calc score
-	useEffect(() => {
-		let calcScore = 0;
-		const correctCats = [];
-		allAnswers.forEach(category => {
-			category.forEach((question, qIndex) => {
-				if(question) calcScore += (qIndex + 1) * 100;
-				if(question && qIndex === 4 && correctCats.length < 6)
-					correctCats.push(100 + correctCats.length * 100);
-				if(correctCats.length === 6) correctCats[5] += 11;
-			})
-		})
-		for (let i in correctCats) calcScore += correctCats[i];
-		calcScore += correctCats.reduce((sum, cat) => sum + cat, 0);
-		setScore(calcScore);
-	}, [allAnswers]);
+  // calc score
+  useEffect(() => {
+    let calcScore = 0;
+    let correctCats = [];
+    for (let i = 0; i < allAnswers.length; i++) {
+      for (let j = 0; j < allAnswers[i].length; j++) {
+        if (allAnswers[i][j] === true) {
+          calcScore += (j + 1) * 100;
+          allRefs.current[i][j].style.color = 'green';
+        }
+      }
+      if (allAnswers[i][4] === true)
+        correctCats.push(100 + correctCats.length * 100);
+    }
+    for (let i in correctCats) calcScore += correctCats[i];
+    setScore(calcScore);
+  }, [allAnswers]);
 
-
-	return (
-		<>
-			<div className={classes.SCORECLASS_HERE}>
-				<p>{score}</p>
-			</div>
-			<div className={classes.GAMECONTAINERCLASS_HERE}>
-				{isLoading && <Spinner />}
-				{showQuestion && <Question />}
-				{!isLoading && !showQuestion &&
-					selectedCategories.map((category, i) => (
-						<Category data={category} index={i} key={i}/>
-					))
-				}
-			</div>
-		</>
-	)
+  return (
+    <>
+      <h2 className='score'>Score: {score}</h2>
+      <div className='game'>
+				{!questions && <Spinner />}
+        {questions && !showQuestion &&
+          selectedCategories.map((category, i) => {
+            return (
+              <div className='category' key={category.id}>
+                <h3>
+                  {category.name.startsWith('Entertainment:')
+                  || category.name.startsWith('Science:')
+                    ? category.name.slice(category.name.indexOf(' ') + 1)
+                    : category.name}
+                </h3>
+                <button
+                  ref={ref => setRef(i, 4, ref)}
+                  onClick={() => setShowQuestion({
+                      question: questions[i][4],
+                      refs: allRefs[i],
+                      category: i,
+                      number: 4,
+                    })
+                  }
+                >500</button>
+                <button
+                  ref={ref => setRef(i, 3, ref)}
+                  onClick={() => setShowQuestion({
+                      question: questions[i][3],
+                      refs: allRefs[i],
+                      category: i,
+                      number: 3,
+                    })
+                  }
+                >400</button>
+                <button
+                  ref={ref => setRef(i, 2, ref)}
+                  onClick={() => setShowQuestion({
+                      question: questions[i][2],
+                      refs: allRefs[i],
+                      category: i,
+                      number: 2,
+                    })
+                  }
+                >300</button>
+                <button
+                  ref={ref => setRef(i, 1, ref)}
+                  onClick={() => setShowQuestion({
+                      question: questions[i][1],
+                      refs: allRefs[i],
+                      category: i,
+                      number: 1,
+                    })
+                  }
+                >200</button>
+                <button
+                  ref={ref => setRef(i, 0, ref)}
+                  onClick={() => setShowQuestion({
+                      question: questions[i][0],
+                      refs: allRefs[i],
+                      category: i,
+                      number: 0,
+                    })
+                  }
+                >100</button>
+              </div>
+            );
+          })}
+        {showQuestion &&
+          <Question
+            showQuestion={showQuestion}
+            setShowQuestion={setShowQuestion}
+            allAnswers={allAnswers}
+            setAllAnswers={setAllAnswers}
+          />
+        }
+      </div>
+    </>
+  );
 }
