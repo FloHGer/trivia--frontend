dfc907781f8fba76a524b6884d0b856e4ebaac90
Tobias Pfaffenzeller <tobler@gmx.com>
2022-02-07 09:56:29 +0100

    upload image

--- src/components/Dashboard/UserProfile/UserInfo/FileUpload.jsx
+++ src/components/Dashboard/UserProfile/UserInfo/FileUpload.jsx
@@ -1,51 +1,108 @@
 import { useState } from "react";
 import axios from "axios";
-
 import { useAuth } from "../../../../context/loginContext";
+// import { blob } from "node:stream/consumers";
+import { RiCloseLine } from "react-icons/ri";
 
 
-const FileUpload = () => {
+const FileUpload = ({ setIsOpen, setImage }) => {
    const [currentUser] = useAuth();
    const [file, setFile] = useState("");
+   const [preview, setPreview] = useState("");
 
-   const fileHandler = (e) => {
-      setFile(e.target.files[0]);
+   const [isSelected, setIsSelected] = useState(false);
+
+   const changeHandler = (e) => {
+      if (e.target.files.length) {
+         setPreview({
+            preview: URL.createObjectURL(e.target.files[0]),
+            raw: e.target.files[0],
+         });
+         setFile(e.target.files[0]);
+      }
+      
+      setIsSelected(true);
+      setIsOpen(true);
    };
 
-   const submitFile = async (e) => {
-      e.preventDefault();
+   // print('Tobi')
+
+   const handleSubmission = async (e) => {
+       e.preventDefault()
+   
       const formData = new FormData();
       formData.append("userImg", file);
-
+      console.log("file", file);
+      console.log("formData", formData);
       try {
          const newImage = await axios.post(
             `${process.env.REACT_APP_BACKEND}/user/${currentUser}/upload`,
-            formData,
-            {
-               headers: {
-                  "Content-Type": "multipart/form-data",
-               },
+             formData,
+             {
+                headers: {
+                   "Content-Type": "multipart/form-data",
+                  },
+               }
+               );
+               console.log(newImage);
+               if (newImage.data.message !== "profile image uploaded")
+               console.log(newImage.data.message);
+               setImage(newImage);
+               setIsOpen(false);
+            } catch (err) {
+               console.log(err);
             }
-         );
-         console.log(newImage)
-         if(newImage.data.message !== 'profile image uploaded') console.log(newImage.data.message)
-      } catch (err) {
-         console.log(err);
-      }
-   };
+            
+            setImage(file);
+            console.log("Modal", file);
+            setIsOpen(true);
+            // closeModal()
+         };
+         
+         
+         
+         return (
+            <>
+               <div onClick={() => setIsOpen(false)} />
+               <div>
+                  <div>
+                     <div>
+                        <h2>Wanna change the picture?</h2>
+                     </div>
+                     {/* CLOSING BUTTON */}
+                     <button onClick={() => setIsOpen(false)}>
+                        <RiCloseLine style={{ marginBottom: "-3px" }} />
+                     </button>
 
-   return (
-      <>
-         <h2>{'Change your Image here'}</h2>
-         <form onSubmit={submitFile}>
-            <label htmlFor="customFile">
-               
-               <input type="file" id="customFile" onChange={fileHandler} />
-            </label>
-            <input type="submit" value="upload new image" />
-         </form>
-      </>
-   );
+                     {/* CONTENT */}
+                     <div>
+                        <input
+                           type="file"
+                           name="file"
+                           onChange={changeHandler}
+                        />
+                        {isSelected ? (
+                           <div>
+                              <img
+                                 src={preview.preview}
+                                 alt="profile-pic"
+                                 style={{ width: "150px", maxHeight: "200px"}}
+                              />
+                           </div>
+                        ) : null}
+                     </div>
+
+                     {/* SUBMIT / CANCEL BUTTONS */}
+                     <div>
+                        <div>
+                           <button onClick={handleSubmission}>Submit</button>
+                           <button onClick={() => setIsOpen(false)}>Cancel</button>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </>
+         );
 };
 
 export default FileUpload;
--- src/components/Dashboard/UserProfile/UserInfo/UserInfoCard.jsx
+++ src/components/Dashboard/UserProfile/UserInfo/UserInfoCard.jsx
@@ -20,6 +20,7 @@ export default function CardUserProfileUser() {
 	const selectRef = useRef();
 	const uploadRef = useRef();
 	const [image, setImage] = useState();
+	const [isOpen, setIsOpen] = useState();
 	
 	useEffect(() => {
 		setEdit(false);
@@ -29,12 +30,13 @@ export default function CardUserProfileUser() {
 		(async () => {
 			const response = (await axios.get(`${process.env.REACT_APP_BACKEND}/user/${currentUser}`)).data;
 			if (response.message === 'success') {
+            console.log(response);
 				setData(response.payload);
 				setUsername(response.payload.username);
 				setEmail(response.payload.email ? response.payload.email : 'enter your email');
-				setImage(response.payload.img)
+            setImage(response.payload.img);
 			}
-		})();
+      })();
 	}, [currentUser, nat, image]);
 	
 	const inputChangeHandler = ({name, value}) => {
@@ -58,86 +60,105 @@ export default function CardUserProfileUser() {
 	};
 	
 	const openImageUpload = e => {
-		e.stopPropagation();
-		if (uploadRef.current.style.visibility === 'visible') return uploadRef.current.style.visibility = 'collapse';
-		selectRef.current.style.visibility = 'collapse';
-		uploadRef.current.style.visibility = 'visible';
+      e.stopPropagation();
+      setIsOpen(true)
+		if(isOpen) {
+         if (uploadRef.current.style.visibility === 'visible') return uploadRef.current.style.visibility = 'collapse';
+		   selectRef.current.style.visibility = 'collapse';
+		   uploadRef.current.style.visibility = 'visible';
+      }
+		
 	}
 	
 
 	return (
-		<section className={classes.profile__user}>
-			<div className={classes['profile__user--piccontainer']}>
-{/* PIC */}
-				<div
-					className={classes['profile__user--pic']}
-					style={{
-						cursor: edit ? 'pointer' : 'default',
-						pointerEvents: edit ? 'auto' : 'none',
-						background: `url(${image || ''}) center / cover no-repeat`,
-					}}
-					onClick={e => openImageUpload(e)}
-				>
-					<div
-							className={classes['profile__user--pic__modal'] }
-							ref = {uploadRef}
-						>
-							<FileUpload />
-						</div>
-{/* FLAGS */}
-					<div
-						className={classes['profile__user--pic__flag']}
-						style={{
-							cursor: edit ? 'pointer' : 'default',
-							pointerEvents: edit ? 'auto' : 'none',
-							background: `url('https://flagcdn.com/64x48/${(data && data.nat) || 'un'}.png') center / cover no-repeat`,
-						}}
-						onClick={e => openFlagsHandler(e)}
-					>
-						
-						<div className={classes.flagSelection} ref={selectRef}>
-							{Object.keys(flags).map((flag, i) => (
-								<label key={flag}>
-									<input
-										type='radio'
-										name='nation'
-										value={flag}
-										onChange={() => flagChangeHandler(flag)}
-										title={flags[flag].name}
-										style={{background: `url(${flags[flag].url.small}) center / cover no-repeat`}}
-									/>
-								</label>
-							))}
-						</div>
-					</div>
-				</div>
-			</div>
-{/* INPUTS */}
-			<div className={classes['profile__user--namecontainer']}>
-				<div className={classes['profile__user--name']}>
-					<input
-						type='text'
-						name='username'
-						value={username || ''}
-						readOnly={edit ? false : true}
-						onChange={e => inputChangeHandler(e.target)}
-						onBlur={e => validation(e.target, currentUser, setCurrentUser)}
-					/>
-				</div>
-				<div className={classes['profile__user--email']}>
-					<input
-						type='text'
-						name='email'
-						value={email || ''}
-						readOnly={edit ? false : true}
-						onChange={e => inputChangeHandler(e.target)}
-						onBlur={e => validation(e.target, currentUser, setCurrentUser)}
-					/>
-				</div>
-			</div>
-			<div className={classes['profile__user--iconcontainer']}>
-				<FaEdit className={classes['profile__user--icon']} style={{color: edit ? 'lime' : 'red'}} title={'Edit profile'} onClick={() => setEdit(!edit)} />
-			</div>
-		</section>
-	);
+      <section className={classes.profile__user}>
+         <div className={classes["profile__user--piccontainer"]}>
+            {/* PIC */}
+            <div
+               className={classes["profile__user--pic"]}
+               style={{
+                  cursor: edit ? "pointer" : "default",
+                  pointerEvents: edit ? "auto" : "none",
+                  background: `url(${image || ""}) center / cover no-repeat`,
+               }}
+			   
+                  onClick={(e) => openImageUpload(e)}
+			   >
+               <div
+                  className={classes["profile__user--pic__modal"]}
+                  ref={uploadRef}
+               >
+                  {isOpen && (
+                     <FileUpload setImage={setImage} setIsOpen={setIsOpen} />
+                  )}
+               </div>
+               {/* FLAGS */}
+               <div
+                  className={classes["profile__user--pic__flag"]}
+                  style={{
+                     cursor: edit ? "pointer" : "default",
+                     pointerEvents: edit ? "auto" : "none",
+                     background: `url('https://flagcdn.com/64x48/${
+                        (data && data.nat) || "un"
+                     }.png') center / cover no-repeat`,
+                  }}
+                  onClick={(e) => openFlagsHandler(e)}
+               >
+                  <div className={classes.flagSelection} ref={selectRef}>
+                     {Object.keys(flags).map((flag, i) => (
+                        <label key={flag}>
+                           <input
+                              type="radio"
+                              name="nation"
+                              value={flag}
+                              onChange={() => flagChangeHandler(flag)}
+                              title={flags[flag].name}
+                              style={{
+                                 background: `url(${flags[flag].url.small}) center / cover no-repeat`,
+                              }}
+                           />
+                        </label>
+                     ))}
+                  </div>
+               </div>
+            </div>
+         </div>
+         {/* INPUTS */}
+         <div className={classes["profile__user--namecontainer"]}>
+            <div className={classes["profile__user--name"]}>
+               <input
+                  type="text"
+                  name="username"
+                  value={username || ""}
+                  readOnly={edit ? false : true}
+                  onChange={(e) => inputChangeHandler(e.target)}
+                  onBlur={(e) =>
+                     validation(e.target, currentUser, setCurrentUser)
+                  }
+               />
+            </div>
+            <div className={classes["profile__user--email"]}>
+               <input
+                  type="text"
+                  name="email"
+                  value={email || ""}
+                  readOnly={edit ? false : true}
+                  onChange={(e) => inputChangeHandler(e.target)}
+                  onBlur={(e) =>
+                     validation(e.target, currentUser, setCurrentUser)
+                  }
+               />
+            </div>
+         </div>
+         <div className={classes["profile__user--iconcontainer"]}>
+            <FaEdit
+               className={classes["profile__user--icon"]}
+               style={{ color: edit ? "lime" : "red" }}
+               title={"Edit profile"}
+               onClick={() => setEdit(!edit)}
+            />
+         </div>
+      </section>
+   );
 }
