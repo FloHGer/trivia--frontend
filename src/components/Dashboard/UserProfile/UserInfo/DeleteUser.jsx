c7716369f4afaa7e05c0d9fccf41dc0e4518d1b7
Tobias Pfaffenzeller <tobler@gmx.com>
2022-02-19 16:53:33 +0100

    add favicon

--- src/components/Dashboard/UserProfile/UserInfo/DeleteUser.jsx
+++ src/components/Dashboard/UserProfile/UserInfo/DeleteUser.jsx
@@ -4,80 +4,75 @@ import axios from "axios";
 import { useAuth } from "../../../../context/loginContext";
 
 import classes from "./DeleteUser.module.scss";
-import FadeOut from '../../../common/FadeOut'
+import FadeOut from "../../../common/FadeOut";
 import Card from "../../../common/Card";
 import Button from "../../../common/Button";
-import styles from '../../../common/Button.module.scss'
-
+import styles from "../../../common/Button.module.scss";
 
 const DeleteUser = ({ setOpenDeleteModal }) => {
-   const [currentUser, setCurrentUser] = useAuth();
-   const [isDeleted, setIsDeleted] = useState()
-   const [finalScreen, setFinalScreen] = useState(false);
-   const navigate = useNavigate();
-   
-   const deleteProfile = async () => {
-      try {
-         const deleteUser = await (
-            await axios.delete(
-               `${process.env.REACT_APP_BACKEND}/user/${currentUser}`
-            )
-         ).data.message;
-         if (deleteUser === "user deleted") {
-            setIsDeleted(true)
-            setCurrentUser(false);
-            
-         }
-      } catch (err) {}
-   };
-   
-   useEffect (()=> {
-      console.log("test2");
-      if (isDeleted) {
-         setFinalScreen(true);
-         setTimeout(() => {
-         
-         return console.log("test");;
-      }, 3000)
-      navigate('/')
-      };
-   }, [isDeleted, navigate])
+    const [currentUser, setCurrentUser] = useAuth();
+    const [isDeleted, setIsDeleted] = useState();
+    const [finalScreen, setFinalScreen] = useState(false);
+    const navigate = useNavigate();
 
-   return (
-      <>
-         <FadeOut onClick={() => setOpenDeleteModal(false)}></FadeOut>
-         <Card className={classes["modal"]}>
-            <h2>Do you really want to delete your profile?</h2>
-            {/* SUBMIT / CANCEL BUTTONS */}
-            <div className={classes["modal__button--container"]}>
-               <Button
-                  className={styles.btn__red}
-                  title={"delete"}
-                  onClick={deleteProfile}
-                  maxWidth="18rem"
-                  maxHeight="6rem"
-                  borderRadius="2rem"
-               ></Button>
-               <Button
-                  className={styles.btn__blue}
-                  title={"cancel"}
-                  maxWidth="18rem"
-                  maxHeight="6rem"
-                  borderRadius="2rem"
-                  onClick={() => setOpenDeleteModal(false)}
-               ></Button>
-            </div>
-         </Card>
-         {finalScreen && (
-            <Card maxWidth={"30%"}>
-               <div>
-                  <h2>{"Congratulations:"}</h2>
-                  <p>{`Your profile was deleted!`}</p>
-               </div>
-            </Card>
-         )}
-      </>
-   );
+    const deleteProfile = async () => {
+        try {
+            const deleteUser = await (
+                await axios.delete(
+                    `${process.env.REACT_APP_BACKEND}/user/${currentUser}`
+                )
+            ).data.message;
+            if (deleteUser === "user deleted") {
+                setFinalScreen(true);
+                setTimeout(() => {
+                    return (
+                        navigate("/"), setIsDeleted(true), setCurrentUser(false)
+                    );
+                }, 3000);
+            }
+        } catch (err) {}
+    };
+
+    return (
+        <>
+            <FadeOut onClick={() => setOpenDeleteModal(false)}></FadeOut>
+            {!finalScreen && (
+                <Card className={classes["modal"]}>
+                    <h2>Do you really want to delete your profile?</h2>
+                    {/* SUBMIT / CANCEL BUTTONS */}
+                    <div className={classes["modal__button--container"]}>
+                        <Button
+                            className={styles.btn__red}
+                            title={"delete"}
+                            onClick={deleteProfile}
+                            maxWidth="18rem"
+                            maxHeight="6rem"
+                            borderRadius="1rem"
+                        ></Button>
+                        <Button
+                            className={styles.btn__blue}
+                            title={"cancel"}
+                            maxWidth="18rem"
+                            maxHeight="6rem"
+                            borderRadius="1rem"
+                            onClick={() => setOpenDeleteModal(false)}
+                        ></Button>
+                    </div>
+                </Card>
+            )}
+            {finalScreen && (
+                <Card className={classes["modal"]}>
+                    <div className={classes["modal__finalScreen"]}>
+                        <h2>{`GOOD BYE`}</h2>
+                        <h2>
+                            <span>{`${currentUser}`}</span>
+                        </h2>
+                        <h2>{`Your profile was successfully deleted!`}</h2>
+                    </div>
+                </Card>
+            )}
+        </>
+    );
 };
 
 export default DeleteUser;
--- src/components/Dashboard/UserProfile/UserInfo/UserInfoCard.jsx
+++ src/components/Dashboard/UserProfile/UserInfo/UserInfoCard.jsx
@@ -5,201 +5,207 @@ import FileUpload from "./FileUpload";
 import { validation } from "../../../../common/inputValidation.js";
 import { flags } from "../../../../common/flags.js";
 import { useAuth } from "../../../../context/loginContext.js";
-import FeedbackCard from '../../../common/FeedbackCard';
+import FeedbackCard from "../../../common/FeedbackCard";
 import DeleteUser from "./DeleteUser";
 
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import classes from "./UserInfoCard.module.scss";
 
 export default function UserInfoCard() {
-   const [currentUser, setCurrentUser] = useAuth();
-   const [response, setResponse] = useState(null);
-   const [data, setData] = useState();
-   const [nat, setNat] = useState();
-   const [username, setUsername] = useState();
-   const [email, setEmail] = useState();
-   const [edit, setEdit] = useState(false);
-   const [image, setImage] = useState();
-   const [flagsModal, setFlagsModal] = useState(false);
-   const [imageModal, setImageModal] = useState(false);
-   const [openDeleteModal, setOpenDeleteModal] = useState();
+    const [currentUser, setCurrentUser] = useAuth();
+    const [response, setResponse] = useState(null);
+    const [data, setData] = useState();
+    const [nat, setNat] = useState();
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [edit, setEdit] = useState(false);
+    const [image, setImage] = useState();
+    const [flagsModal, setFlagsModal] = useState(false);
+    const [imageModal, setImageModal] = useState(false);
+    const [openDeleteModal, setOpenDeleteModal] = useState();
 
-   useEffect(() => {
-      setEdit(false);
-   }, []);
+    useEffect(() => {
+        setEdit(false);
+    }, []);
 
-   useEffect(() => {
-      (async () => {
-         const response = (
-            await axios.get(
-               `${process.env.REACT_APP_BACKEND}/user/${currentUser}`
-               )
-               ).data;
-               if (response.message === "success") {
-                  setData(response.payload);
-                  setUsername(response.payload.username);
-                  setEmail(
-                     response.payload.email
-                     ? response.payload.email
-                     : "enter your email"
-                     );
-                  setImage(response.payload.img);
-               }
-            })();
-   }, [currentUser, nat, imageModal]);
+    useEffect(() => {
+        (async () => {
+            const response = (
+                await axios.get(
+                    `${process.env.REACT_APP_BACKEND}/user/${currentUser}`
+                )
+            ).data;
+            if (response.message === "success") {
+                setData(response.payload);
+                setUsername(response.payload.username);
+                setEmail(
+                    response.payload.email
+                        ? response.payload.email
+                        : "enter your email"
+                );
+                setImage(response.payload.img);
+            }
+        })();
+    }, [currentUser, nat, imageModal]);
 
-   const inputChangeHandler = ({ name, value }) => {
-      name === "username" ? setUsername(value) : setEmail(value);
-   };
+    const inputChangeHandler = ({ name, value }) => {
+        name === "username" ? setUsername(value) : setEmail(value);
+    };
 
-   const flagChangeHandler = async (key) => {
-      try {
-         setFlagsModal(false);
-         const update = (
-            await axios.patch(
-               `${process.env.REACT_APP_BACKEND}/user/${currentUser}`,
-               { updates: { nat: key } }
-            )
-         ).data.message;
-         if (update === "user updated") return setNat(data.nat);
-      } catch (err) {}
-   };
+    const flagChangeHandler = async (key) => {
+        try {
+            setFlagsModal(false);
+            const update = (
+                await axios.patch(
+                    `${process.env.REACT_APP_BACKEND}/user/${currentUser}`,
+                    { updates: { nat: key } }
+                )
+            ).data.message;
+            if (update === "user updated") return setNat(data.nat);
+        } catch (err) {}
+    };
 
-   const openFlagsHandler = (e) => {
-      e.stopPropagation();
-      setImageModal(false);
-      setFlagsModal(!flagsModal);
-   };
+    const openFlagsHandler = (e) => {
+        e.stopPropagation();
+        setImageModal(false);
+        setFlagsModal(!flagsModal);
+    };
 
-   const openImageUpload = (e) => {
-      e.stopPropagation();
-      setFlagsModal(false);
-      setImageModal(!imageModal);
-   };
+    const openImageUpload = (e) => {
+        e.stopPropagation();
+        setFlagsModal(false);
+        setImageModal(!imageModal);
+    };
 
-   return (
-       <section className={classes.profile__user}>
-           {response && 
-               <FeedbackCard
-                   title={"SUCCESS"}
-                   text={response}
-                   onClick={() => setResponse(null)}
-               />
-           }
-           {/* PIC */}
-           <div className={classes["profile__user--piccontainer"]}>
-               <img
-                   key={Date.now()}
-                   src={image || ""}
-                   alt={currentUser}
-                   referrerPolicy="no-referrer"
-                   className={classes["profile__user--pic"]}
-                   style={{
-                       cursor: edit ? "pointer" : "default",
-                       pointerEvents: edit ? "auto" : "none",
-                   }}
-                   onClick={(e) => openImageUpload(e)}
-               />
-               {imageModal && (
-                   <FileUpload
-                       setImage={setImage}
-                       setImageModal={setImageModal}
-                   />
-               )}
+    const modalHandler = () => {
+        setOpenDeleteModal(true);
+        setFlagsModal(false);
+        setImageModal(false);
+    };
 
-               {/* FLAGS */}
-               <div
-                   className={classes["profile__user--pic__flag"]}
-                   style={{
-                       cursor: edit ? "pointer" : "default",
-                       pointerEvents: edit ? "auto" : "none",
-                       background: `url('https://flagcdn.com/64x48/${
-                           (data && data.nat) || "un"
-                       }.png') center / contain no-repeat`,
-                   }}
-                   onClick={(e) => openFlagsHandler(e)}
-               >
-                   {flagsModal && (
-                       <div className={classes.flagSelection}>
-                           {Object.keys(flags).map((flag, i) => (
-                               <label key={flag}>
-                                   <input
-                                       type="radio"
-                                       name="nation"
-                                       value={flag}
-                                       onChange={() => flagChangeHandler(flag)}
-                                       title={flags[flag].name}
-                                       style={{
-                                           background: `url(${flags[flag].url.small}) center / contain no-repeat`,
-                                       }}
-                                   />
-                               </label>
-                           ))}
-                       </div>
-                   )}
-               </div>
-           </div>
-           {/* INPUTS */}
-           <div className={classes["profile__user--namecontainer"]}>
-               <div className={classes["profile__user--name"]}>
-                   <input
-                       type="text"
-                       name="username"
-                       value={username || ""}
-                       readOnly={edit ? false : true}
-                       onChange={(e) => inputChangeHandler(e.target)}
-                       onBlur={(e) =>
-                           edit
-                               ? validation(
-                                     e.target,
-                                     currentUser,
-                                     setCurrentUser,
-                                     setResponse
-                                 )
-                               : null
-                       }
-                   />
-               </div>
-               <div className={classes["profile__user--email"]}>
-                   <input
-                       type="text"
-                       name="email"
-                       value={email || ""}
-                       readOnly={edit ? true : true}
-                       onChange={(e) => inputChangeHandler(e.target)}
-                       onBlur={(e) =>
-                           edit
-                               ? validation(
-                                     e.target,
-                                     currentUser,
-                                     setCurrentUser,
-                                     setResponse
-                                 )
-                               : null
-                       }
-                   />
-               </div>
-           </div>
-           <div className={classes["profile__user--iconcontainer"]}>
-               <FaEdit
-                   className={classes["profile__user--editIcon"]}
-                   style={{ color: edit ? "lime" : "red" }}
-                   title={"Edit profile"}
-                   onClick={() => setEdit(!edit)}
-               />
+    return (
+        <section className={classes.profile__user}>
+            {response && (
+                <FeedbackCard
+                    title={"SUCCESS"}
+                    text={response}
+                    onClick={() => setResponse(null)}
+                />
+            )}
+            {/* PIC */}
+            <div className={classes["profile__user--piccontainer"]}>
+                <img
+                    key={Date.now()}
+                    src={image || ""}
+                    alt={currentUser}
+                    referrerPolicy="no-referrer"
+                    className={classes["profile__user--pic"]}
+                    style={{
+                        cursor: edit ? "pointer" : "default",
+                        pointerEvents: edit ? "auto" : "none",
+                    }}
+                    onClick={(e) => openImageUpload(e)}
+                />
+                {imageModal && (
+                    <FileUpload
+                        setImage={setImage}
+                        setImageModal={setImageModal}
+                    />
+                )}
 
-               <FaTrashAlt
-                   className={classes["profile__user--trashIcon"]}
-                   style={{
-                       visibility: edit ? "visible" : "hidden",
-                   }}
-                   title={"Delete profile"}
-                   onClick={() => setOpenDeleteModal(true)}
-               />
-           </div>
-           {openDeleteModal && (
-               <DeleteUser setOpenDeleteModal={setOpenDeleteModal} />
-           )}
-       </section>
-   );
+                {/* FLAGS */}
+                <div
+                    className={classes["profile__user--pic__flag"]}
+                    style={{
+                        cursor: edit ? "pointer" : "default",
+                        pointerEvents: edit ? "auto" : "none",
+                        background: `url('https://flagcdn.com/64x48/${
+                            (data && data.nat) || "un"
+                        }.png') center / contain no-repeat`,
+                    }}
+                    onClick={(e) => openFlagsHandler(e)}
+                >
+                    {flagsModal && (
+                        <div className={classes.flagSelection}>
+                            {Object.keys(flags).map((flag, i) => (
+                                <label key={flag}>
+                                    <input
+                                        type="radio"
+                                        name="nation"
+                                        value={flag}
+                                        onChange={() => flagChangeHandler(flag)}
+                                        title={flags[flag].name}
+                                        style={{
+                                            background: `url(${flags[flag].url.small}) center / contain no-repeat`,
+                                        }}
+                                    />
+                                </label>
+                            ))}
+                        </div>
+                    )}
+                </div>
+            </div>
+            {/* INPUTS */}
+            <div className={classes["profile__user--namecontainer"]}>
+                <div className={classes["profile__user--name"]}>
+                    <input
+                        type="text"
+                        name="username"
+                        value={username || ""}
+                        readOnly={edit ? false : true}
+                        onChange={(e) => inputChangeHandler(e.target)}
+                        onBlur={(e) =>
+                            edit
+                                ? validation(
+                                      e.target,
+                                      currentUser,
+                                      setCurrentUser,
+                                      setResponse
+                                  )
+                                : null
+                        }
+                    />
+                </div>
+                <div className={classes["profile__user--email"]}>
+                    <input
+                        type="text"
+                        name="email"
+                        value={email || ""}
+                        readOnly={edit ? true : true}
+                        onChange={(e) => inputChangeHandler(e.target)}
+                        onBlur={(e) =>
+                            edit
+                                ? validation(
+                                      e.target,
+                                      currentUser,
+                                      setCurrentUser,
+                                      setResponse
+                                  )
+                                : null
+                        }
+                    />
+                </div>
+            </div>
+            <div className={classes["profile__user--iconcontainer"]}>
+                <FaEdit
+                    className={classes["profile__user--editIcon"]}
+                    style={{ color: edit ? "lime" : "red" }}
+                    title={"Edit profile"}
+                    onClick={() => setEdit(!edit)}
+                />
+
+                <FaTrashAlt
+                    className={classes["profile__user--trashIcon"]}
+                    style={{
+                        visibility: edit ? "visible" : "hidden",
+                    }}
+                    title={"Delete profile"}
+                    onClick={modalHandler}
+                />
+            </div>
+            {openDeleteModal && (
+                <DeleteUser setOpenDeleteModal={setOpenDeleteModal} />
+            )}
+        </section>
+    );
 }
