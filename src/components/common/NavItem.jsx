36571e0666914238725f84367e9a5afdff212d34
FloHGer <mail@florianhoehle.de>
2022-02-23 19:02:28 +0100

    Rework navitem

--- src/components/common/NavItem.jsx
+++ src/components/common/NavItem.jsx
@@ -6,11 +6,11 @@ export default function NavItem({title, target, number, back}) {
   return (
     <li className={classes.listItem}>
       <div className={`${classes.listItem__line} ${classes[`listItem__line__${number}`]}`}></div>
-      <div className={`${classes.listItem__diamond}`}></div>
       <Link to={`${target}`}>
-        <h2 className={`heading heading__2 ${classes.listItem__link}  ${classes[back]}`}>
-          {title}
-        </h2>
+        <div className={classes.listItem__link}>
+          <div className={`${classes.listItem__link__diamond}`}></div>
+          <h2 className={`heading heading__2 ${classes[back]}`}>{title}</h2>
+        </div>
       </Link>
     </li>
   );
