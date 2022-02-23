6e258b21176ac700671d8b425ff8c1c6e45edb3b
FloHGer <mail@florianhoehle.de>
2022-02-23 18:51:05 +0100

    Save navitem

--- src/components/common/NavItem.jsx
+++ src/components/common/NavItem.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
 
 import classes from "./NavItem.module.scss";
-import cn from "classnames";
 
 export default function NavItem({title, target, number, back}) {
   return (
-    <li className={classes["navigation__list--item"]}>
-      <div className={`line line__${number}`}></div>
-      <div className="diamond"></div>
+    <li className={classes.listItem}>
+      <div className={`${classes.listItem__line} ${classes[`listItem__line__${number}`]}`}></div>
+      <div className={`${classes.listItem__diamond}`}></div>
       <Link to={`${target}`}>
-        <h2 className={cn(classes["navigation__list--link"], "heading heading__2", classes[back])}>
+        <h2 className={`heading heading__2 ${classes.listItem__link}  ${classes[back]}`}>
           {title}
         </h2>
       </Link>
